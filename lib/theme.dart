10f797669f329cb855631b10f6ac805e2560fe66
Mohammed Safwan <mdsafwan3102001@gmail.com>
2021-08-30 20:55:51 +0530

    First Commit

---
+++ lib/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const primary_color = Color(0xff2377f4);
+const secondary_color = Color(0xffFF884B);
+const field_color = Color(0xffE5E5E5);
+
+ThemeData theme(){
+  return ThemeData(primaryColor: primary_color);
+}
