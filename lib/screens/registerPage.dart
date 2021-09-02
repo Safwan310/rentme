a2c70ad417b3e977dd479961f53c2dd21d3469d7
Menaga M <64275044+Menaga1422@users.noreply.github.com>
2021-09-02 10:48:35 +0530

    Add files via upload

--- lib/screens/registerPage.dart
+++ lib/screens/registerPage.dart
@@ -2,6 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:rent_my_stuff/screens/loginPage.dart';
 import 'package:rent_my_stuff/backend/authentication';
+import 'package:rent_my_stuff/theme.dart';
+
+// import '../theme.dart';
 
 import 'Home.dart';
 
@@ -22,100 +25,128 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: primary_color,
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.fromLTRB(30,150,30,0),
+          padding: EdgeInsets.fromLTRB(30,50,30,0),
           child: Column(
             children: [
-              Text("Register"),
-              SizedBox(height: 65,),
-              Form(
-                  key: formGlobalKey,
-                  child: Column(
-                    children: [
+              Text("Register",style: Theme.of(context).textTheme.headline1!.copyWith(fontSize: 48,color: Colors.white),),
+              // SizedBox(height: 65,),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                margin: EdgeInsets.symmetric(vertical: 55, horizontal: 20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Theme.of(context).hintColor.withOpacity(0.2),
+                          offset: Offset(0, 10),
+                          blurRadius: 20)]
+                ),
+                child: Form(
+                    key: formGlobalKey,
+                    child: Column(
+                      children: [
 
-                      TextFormField(
-                        controller: _username,
-                        decoration: InputDecoration(
-                            labelText: "Username"
+                        TextFormField(
+                          controller: _username,
+                          decoration: InputDecoration(
+                              labelText: "Username"
+                          ),
+                          validator: (uname){
+                            if(uname!.isEmpty){
+                              return 'Username cant be empty';
+                            }
+                          },
                         ),
-                        validator: (uname){
-                          if(uname!.isEmpty){
-                            return 'Username cant be empty';
-                          }
-                        },
-                      ),
-                      SizedBox(height: 20,),
-                      TextFormField(
-                        controller: _email,
-                        decoration: InputDecoration(
-                            labelText: "Email"
+                        // SizedBox(height: 20,),
+                        TextFormField(
+                          controller: _email,
+                          decoration: InputDecoration(
+                              labelText: "Email"
+                          ),
+                          validator: (email){
+                            if(email!.isEmpty){
+                              return 'Enter a valid email';
+                            }
+                          },
                         ),
-                        validator: (email){
-                          if(email!.isEmpty){
-                            return 'Enter a valid email';
-                          }
-                        },
-                      ),
-                      SizedBox(height: 20,),
-                      TextFormField(
-                        controller: _pwd,
-                        decoration: InputDecoration(
-                            labelText: "Password"
+                        // SizedBox(height: 20,),
+                        TextFormField(
+                          controller: _pwd,
+                          decoration: InputDecoration(
+                              labelText: "Password"
+                          ),
+                          validator: (pwd){
+                            if(pwd!.isEmpty){
+                              return 'Password cant be empty';
+                            }
+                          },
                         ),
-                        validator: (pwd){
-                          if(pwd!.isEmpty){
-                            return 'Password cant be empty';
-                          }
-                        },
-                      ),
-                      SizedBox(height: 20,),
-                      TextFormField(
-                        controller: _cpwd,
-                        decoration: InputDecoration(
-                            labelText: "Confirm Password"
+                        // SizedBox(height: 20,),
+                        TextFormField(
+                          controller: _cpwd,
+                          decoration: InputDecoration(
+                              labelText: "Confirm Password"
+                          ),
+                          validator: (cpwd){
+                            if(cpwd != _pwd.text){
+                              return 'Passwords dont match';
+                            }
+                          },
                         ),
-                        validator: (cpwd){
-                          if(cpwd != _pwd.text){
-                            return 'Passwords dont match';
-                          }
-                        },
-                      ),
-                      SizedBox(height: 65),
-                      Container(
-                        height: 50,
-                        width: MediaQuery.of(context).size.width/1.2,
-                        child: ElevatedButton( onPressed: (){
-                          if (formGlobalKey.currentState!.validate()) {
-                            formGlobalKey.currentState!.save();
-                            // use the email provided here
-                            AuthenticationHelper()
-                                .signUp(email: _email.text, password: _pwd.text)
-                                .then((result) {
-                              if (result == null) {
-                                final FirebaseAuth auth = FirebaseAuth.instance;
-                                final User? user = auth.currentUser;
-                                final uid = user!.uid;
-                                //print(user.toString()+"=="+uid);
-                                Navigator.pushReplacement(context,
-                                    MaterialPageRoute(builder: (context) => Home(uid)));
-                              } else {
+                        SizedBox(height: 45),
+                        Container(
+                          height: 50,
+                          width: MediaQuery.of(context).size.width/1.5,
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: secondary_color,
+                                elevation: 10.0,
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
+                              ),
+                              onPressed: (){
+                                if (formGlobalKey.currentState!.validate()) {
+                                  formGlobalKey.currentState!.save();
+                                  // use the email provided here
+                                  AuthenticationHelper()
+                                      .signUp(email: _email.text, password: _pwd.text)
+                                      .then((result) {
+                                    if (result == null) {
+                                      final FirebaseAuth auth = FirebaseAuth.instance;
+                                      final User? user = auth.currentUser;
+                                      final uid = user!.uid;
+                                      //print(user.toString()+"=="+uid);
+                                      Navigator.pushReplacement(context,
+                                          MaterialPageRoute(builder: (context) => Home(uid)));
+                                    } else {
+                                      Scaffold.of(context).showSnackBar(SnackBar(
+                                        content: Text(
+                                          result,
+                                          style: TextStyle(fontSize: 16),
+                                        ),
+                                      ));
+                                    }
+                                  });
+                                }
+                              },
 
-                              }
-                            });
-                          }
-                        },
-                            child: Text("Register")),
-                      ),
-                      TextButton(onPressed: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage()));
-                      }, child: Text("Already a user? Log in")),
-                    ],
-                  )
+                              child: Text("Register",style: Theme.of(context).textTheme.button!.copyWith(fontSize:18,color: Colors.white),)),
+
+                        ),
+                        TextButton(onPressed: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage()));
+                        }, child: Text("Already a user? Log in",style: Theme.of(context).textTheme.bodyText1,)),
+                      ],
+                    )
+                ),
               )
             ],
           ),
         ),
       ),
     );
-}}
+  }}
